1053b5d5dc2f59671d3519185556905934dc3278
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-01-08 00:30:29 +0900

    Feat : 소켓 (GET)addPeers 기능 추가

--- server/networks.js
+++ server/networks.js
@@ -1,23 +1,26 @@
 /* P2P Server (노드와 노드 간의 통신) */
-const p2p_port = process.env.P2P_PORT || 6001;
-
 const WebSocket = require("ws");
+const p2p_port = process.env.P2P_PORT || 7001;
 
 //wsinit
 function initP2PServer(test_port) {
   const server = new WebSocket.Server({ port: test_port });
-  //console.log(server);
   server.on("connection", (ws) => {
     initConnection(ws);
   });
   console.log("Listening webSocket port : " + test_port);
 }
 
-initP2PServer(6001);
-initP2PServer(6002);
-initP2PServer(6003);
+initP2PServer(7001);
+initP2PServer(7002);
+initP2PServer(7003);
 
 let sockets = [];
+
+function initConnection(ws) {
+  console.log(ws._socket.remotePort);
+  sockets.push(ws);
+}
 
 function getSockets() {
   return sockets;
@@ -36,7 +39,6 @@ function broadcast(message) {
 function connectToPeers(newPeers) {
   newPeers.forEach((peer) => {
     const ws = new WebSocket(peer);
-    console.log(ws);
     ws.on("open", () => {
       console.log("open");
       initConnection(ws);
@@ -47,16 +49,96 @@ function connectToPeers(newPeers) {
   });
 }
 
-function initConnection(ws) {
-  //console.log(ws._socket.remotePort)
-  sockets.push(ws);
+// const MessageType = {
+//   QUERY_LATEST: 0,
+//   QUERY_ALL: 1,
+//   RESPONSE_BLOCKCHAIN: 2,
+// };
+
+// // //
+// function initMessageHandler(ws) {
+//   ws.on("message", (data) => {
+//     const message = JSON.parse(data);
+//     switch (message.type) {
+//       case MessageType.QUERY_LATEST:
+//         write(ws, responseLatestMsg());
+//         break;
+//       case MessageType.QUERY_ALL:
+//         write(ws, responseAllChainMsg());
+//         break;
+//       case MessageType.RESPONSE_BLOCKCHAIN:
+//         handleBlockChainResponse(message);
+//         break;
+//     }
+//   });
+// }
+
+// function responseLatestMsg() {
+//   return {
+//     type: RESPONSE_BLOCKCHAIN,
+//     data: JSON.stringify([getLastBlock()]),
+//   };
+// }
+// function responseAllChainMsg() {
+//   return {
+//     type: RESPONSE_BLOCKCHAIN,
+//     data: JSON.stringify(getBlocks()),
+//   };
+// }
+
+// function handleBlockChainResponse(message) {
+//   const receiveBlocks = JSON.parse(message.data);
+//   const latestReceiveBlock = receiveBlocks[receiveBlocks.length - 1];
+//   const latestMyBlock = getLastBlock();
+
+//   if (latestReceiveBlock.header.index > latestMyBlock.header.index) {
+//     if (createHash(latestMyBlock) === latestReceiveBlock.header.previousHash) {
+//       if (addBlock(latestReceiveBlock)) {
+//         broadcast(responseLastestMsg());
+//       } else {
+//         console.log("Invaild Block!!");
+//       }
+//     } else if (receiveBlocks.length === 1) {
+//       broadcast(queryAllMsg());
+//     } else {
+//       replaceChain(receiveBlocks);
+//     }
+//   } else {
+//     console.log("Do nothing.");
+//   }
+// }
+
+// function queryAllMsg() {
+//   return {
+//     type: QUERY_ALL,
+//     data: null,
+//   };
+// }
+
+// function queryLatestMsg() {
+//   return {
+//     type: QUERY_LATEST,
+//     data: null,
+//   };
+// }
+
+function initErrorHandler(ws) {
+  ws.on("close", () => {
+    closeConnection(ws);
+  });
+  ws.on("error", () => {
+    closeConnection(ws);
+  });
+}
+
+function closeConnection(ws) {
+  console.log(`Connection close${ws.url}`);
+  sockets.splice(sockets.indexOf(ws), 1);
 }
 
 module.exports = {
-  sockets,
-  getSockets,
-  broadcast,
   connectToPeers,
+  getSockets,
   initConnection,
 };
 // const MessageType = {};
