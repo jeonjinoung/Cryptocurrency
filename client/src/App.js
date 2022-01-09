2550befb7be80d3ecea38b975f1e928e0868381b
Park Jun Hyuk <junhyeg08@gmail.com>
2022-01-09 15:36:48 +0900

    Feat: 클라이언트 라우트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,11 @@ import { useSelector } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
 import { StyledEngineProvider } from '@mui/material';
 
-// routing
-import Routes from './routes';
-
 import MainLayout from './layout';
 
 // defaultTheme
 import themes from './themes';
+import { Route, Routes } from 'react-router-dom';
 
 
 // ==============================|| APP ||============================== //
@@ -20,8 +18,9 @@ const App = () => {
     return (
         <StyledEngineProvider injectFirst>
             <ThemeProvider theme={themes(customization)}>
-                {/* <Routes /> */}
-                <MainLayout />
+                <Routes>
+                    <Route path="/" element={<MainLayout />} />
+                </Routes>
             </ThemeProvider>
         </StyledEngineProvider>
     );
