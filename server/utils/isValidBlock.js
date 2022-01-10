bddaaf3c8f176f18ed0d0b85de730dd25405b397
Park Jun Hyuk <junhyeg08@gmail.com>
2022-01-10 22:31:47 +0900

    Feat: 난이도, 시간 검증

--- server/utils/isValidBlock.js
+++ server/utils/isValidBlock.js
@@ -1,56 +1,74 @@
 const merkle = require("merkle");
-const { Blocks, getLastBlock, getDifficulty } = require("../blockchain/blocks");
+const { Block, BlockHeader } = require("../blockchain/blockclass");
+const { Blocks, isValidTimestamp, getLastBlock, getDifficulty, hashMatchesDifficulty } = require("../blockchain/blocks");
 const { createHash } = require("./hash");
 
 function isValidBlockStructure(block) {
-  return (
-    typeof block.header.version === "string" && 
-    typeof block.header.index === "number" && 
-    typeof block.header.previousHash === "string" && //그전 해시값인지
-    typeof block.header.timestamp === "number" && //만든시간
-    typeof block.header.merkleRoot === "string" && //머클루트
-    typeof block.body === "object"
-  ); //body데이터
+  return typeof(block.header.version) === 'string'
+    && typeof(block.header.index) === 'number'
+    && typeof(block.header.previousHash) === 'string'
+    && typeof(block.header.timestamp) === 'number'
+    && typeof(block.header.merkleRoot) === 'string'
+    && typeof(block.header.difficulty) === 'number'
+    && typeof(block.header.nonce) === 'number'
+    && typeof(block.body) === 'object'
 }
 
 function isValidNewBlock(newBlock, previousBlock) {
   if (isValidBlockStructure(newBlock) === false) {
-    //새로운블럭이 잘못만든 실패면
-    console.log("invalid Block Structure");
+    console.log('Invalid Block Structure');
     return false;
   } else if (newBlock.header.index !== previousBlock.header.index + 1) {
-    //새로만든 블럭이랑 그전블럭에 인덱스가 같지않은면
-    console.log("invalid index");
+    console.log('Invalid Index');
     return false;
   } else if (createHash(previousBlock) !== newBlock.header.previousHash) {
-    //이전해시값 비교
-    console.log("invalid previousHash");
+    console.log('Invalid previousHash');
     return false;
-  } else if (
-    (newBlock.body.length === 0 &&
-      "0".repeat(64) !== newBlock.header.merkleRoot) ||
-    (newBlock.body.length !== 0 &&
-      merkle("sha256").sync(newBlock.body).root() !==
-        newBlock.header.merkleRoot)
-  ) {
-    console.log("invalid merkleRoot");
+  } else if (newBlock.body.length === 0 && ('0'.repeat(64) !== newBlock.header.merkleRoot) ||
+    newBlock.body.length !== 0 && (merkle("sha256").sync(newBlock.body).root() !== newBlock.header.merkleRoot)) {
+      console.log('Invalid merkleRoot');
+      return false;
+  } else if (!isValidTimestamp(newBlock, previousBlock)) {
+    console.log("Invalid Timestamp");
+    return false;
+  } else if (!hashMatchesDifficulty(createHash(newBlock), newBlock.header.difficulty)) {
+    console.log("Invalid hash");
     return false;
   }
+  
   return true;
 }
 
+/* 순환 에러 */
+function isValidChain(newBlocks) {
+  if(JSON.stringify(newBlocks[0]) !== JSON.stringify(Blocks[0])) {
+    return false;
+  };
+
+  var tempBlocks = [newBlocks[0]];
+  for (var i = 0; i < newBlocks.length; i++) {    
+    if (isValidNewBlock(newBlocks[i], tempBlocks[i - 1])) {
+      tempBlocks.push(newBlocks[i]);
+    } else {
+      return false;
+    }
+  };
+  return true;
+};
+
 function addBlock(newBlock) {
   if (isValidNewBlock(newBlock, getLastBlock())) {
-    console.log(33333333333333)
-    console.log(newBlock)
-    console.log(2222222222222)
+    const { version, index, previousHash, timestamp, merkleRoot, nonce } = newBlock.header;
+    const { body } = newBlock;
 
-    Blocks.push(newBlock);
-    const difficultyBlock = getDifficulty(Blocks);
-    console.log(77777777);
-    console.log(difficultyBlock);
-    console.log(88888888);
+    const newDifficulty = getDifficulty(Blocks);
+    console.log("난이도", newDifficulty);
     
+    const header = new BlockHeader(version, index, previousHash, timestamp, merkleRoot, newDifficulty, nonce)
+    const newDifficultyBlock = new Block(header, body)
+
+    Blocks.push(newDifficultyBlock);
+
     return true;
   }
   return false;
