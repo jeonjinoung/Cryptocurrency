e34984edd55baac642618501f6125e2634fadfee
jeonjinoung <jeonjinyoung4321@gmail.com>
2022-01-27 15:29:43 +0900

    Feat : node 3단 분할 이제 프론트 시작

---
+++ server/node3/models/user.js
@@ -0,0 +1,39 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        name: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: true,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+        pw: {
+          type: Sequelize.STRING(100), 
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING(40),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: "User",
+        tableName: "users",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {}
+};
